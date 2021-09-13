168513c0c67ac070cf05efe019e19419828274fd
nowshad-netizen <lewsedmond@gmail.com>
2021-09-13 20:07:38 +0600

    add some style

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products`;
-  const localdb = 'http://127.0.0.1:5500/db.json';
+  // API 
+  // const url = `https://fakestoreapi.com/products`;
+  // const localdb = 'http://127.0.0.1:5500/db.json';
   const apiupdate = 'https://raw.githubusercontent.com/ProgrammingHero1/ranga-store-api/main/ranga-api.json?fbclid=IwAR1pTCkjwMHUr0aRzeajNsWCevk6pKTBU2VtP60XAIemJ9RSXvUrzeG5eoU'
   fetch(apiupdate)
     .then((response) => response.json())
@@ -24,12 +25,18 @@ const showProducts = (products) => {
       <p>${product.title}</p>
       </div>
       <p>Category: ${product.category}</p>
+      <h5 calss="prices">Price: $ ${product.price}</h5>
       <div class="rating">
-      <p>Total Rating: <span class="total-rating-point"> ${product.rating.count}<span></p>
-      <p >Rating: <span class="total-rating-point">  ${product.rating.rate} </span></p>
+      <p>Total Rating Count: <span class="total-rating-point"> ${product.rating.count}<span></p>
+      <p >Rating Rate: <span class="total-rating-point">  ${product.rating.rate} </span></p>
       </div>
-      <h3>Price: $ ${product.price}</h3>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="btn btn-outline-success">add to cart</button>
+      
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="btn btn-warning">add to cart
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg>
+      
+      </button>
       <button id="details-btn" class="btn btn-outline-info" >Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
@@ -43,7 +50,7 @@ const addToCart = (id, price) => {
   updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
-
+// Get Input Values 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
   const converted = parseFloat(element);
